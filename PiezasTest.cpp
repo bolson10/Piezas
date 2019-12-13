2b457b4f0f97223eab2668fcb45d2cb01fdceabb
bolson10 <bolson10@mail.csuchico.edu>
2019-12-13 10:11:42 -0800

    Added extra tests for various functions in Pieza class

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,17 +14,24 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-TEST(PiezasTest, sanityCheck)
+TEST(PiezasTest, check_if_dropPiece_returns_X)
 {
-	ASSERT_TRUE(true);
+  Piezas game;
+  ASSERT_EQ(game.dropPiece(0),X);
 }
-TEST(PiezasTest, check_if_dropPiece_catches_out_of_bounds_value_O)
+TEST(PiezasTest, check_if_dropPiece_returns_O)
+{
+  Piezas game;
+  game.dropPiece(0);
+  ASSERT_EQ(game.dropPiece(0),O);
+}
+TEST(PiezasTest, check_if_dropPiece_catches_out_of_bounds_value_low)
 {
 	Piezas game;
-	ASSERT_EQ(game.dropPiece(4),Invalid);
+	ASSERT_EQ(game.dropPiece(-2),Invalid);
 }
 
-TEST(PiezasTest, check_if_dropPiece_catches_out_of_bounds_value_X)
+TEST(PiezasTest, check_if_dropPiece_catches_out_of_bounds_value_high)
 {
   Piezas game;
   game.dropPiece(0);
@@ -39,20 +46,24 @@ TEST(PiezasTest, check_if_dropPiece_returns_blank_on_full_column)
 	game.dropPiece(0);
 	ASSERT_EQ(game.dropPiece(0),Blank);
 }
-TEST(PiezasTest, check_if_pieceAt_catches_out_of_bounds_value)
+TEST(PiezasTest, check_if_pieceAt_catches_out_of_bounds_value_high)
 {
 	Piezas game;
 	ASSERT_EQ(game.pieceAt(4,4),Invalid);
+}
+TEST(PiezasTest, check_if_pieceAt_catches_out_of_bounds_value_low)
+{
+  Piezas game;
+  ASSERT_EQ(game.pieceAt(-1,-1),Invalid);
 }
 
 TEST(PiezasTest, check_if_dropPiece_works_row0_col0)
 {
 	Piezas game;
-	game.dropPiece(0);
-	ASSERT_EQ(game.pieceAt(0,0),X);
+	ASSERT_EQ(game.dropPiece(0),X);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row1_col0)
+TEST(PiezasTest, check_if_pieceAt_works_row1_col0)
 {
 	Piezas game;
 	game.dropPiece(0);
@@ -60,7 +71,7 @@ TEST(PiezasTest, check_if_dropPiece_works_row1_col0)
 	ASSERT_EQ(game.pieceAt(1,0),O);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row2_col0)
+TEST(PiezasTest, check_if_pieceAt_works_row2_col0)
 {
 	Piezas game;
 	game.dropPiece(0);
@@ -69,14 +80,14 @@ TEST(PiezasTest, check_if_dropPiece_works_row2_col0)
 	ASSERT_EQ(game.pieceAt(2,0),X);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row0_col1)
+TEST(PiezasTest, check_if_pieceAt_works_row0_col1)
 {
 	Piezas game;
 	game.dropPiece(1);
 	ASSERT_EQ(game.pieceAt(0,1),X);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row1_col1)
+TEST(PiezasTest, check_if_pieceAt_works_row1_col1)
 {
 	Piezas game;
 	game.dropPiece(1);
@@ -84,7 +95,7 @@ TEST(PiezasTest, check_if_dropPiece_works_row1_col1)
 	ASSERT_EQ(game.pieceAt(1,1),O);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row2_col1)
+TEST(PiezasTest, check_if_pieceAt_works_row2_col1)
 {
 	Piezas game;
 	game.dropPiece(1);
@@ -93,14 +104,14 @@ TEST(PiezasTest, check_if_dropPiece_works_row2_col1)
 	ASSERT_EQ(game.pieceAt(2,1),X);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row0_col2)
+TEST(PiezasTest, check_if_pieceAt_works_row0_col2)
 {
 	Piezas game;
 	game.dropPiece(2);
 	ASSERT_EQ(game.pieceAt(0,2),X);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row1_col2)
+TEST(PiezasTest, check_if_pieceAt_works_row1_col2)
 {
 	Piezas game;
 	game.dropPiece(2);
@@ -108,7 +119,7 @@ TEST(PiezasTest, check_if_dropPiece_works_row1_col2)
 	ASSERT_EQ(game.pieceAt(1,2),O);
 }
 
-TEST(PiezasTest, check_if_dropPiece_works_row2_col2)
+TEST(PiezasTest, check_if_pieceAt_works_row2_col2)
 {
 	Piezas game;
 	game.dropPiece(2);
@@ -150,41 +161,40 @@ TEST(PiezasTest, check_if_gameState_works_win_O)
   ASSERT_EQ(game.gameState(),O);
 }
 
-/*
+
 TEST(PiezasTest, check_if_gameState_works_win_X)
 {
   Piezas game;
-  game.dropPiece(0);
+  game.dropPiece(3);
+  game.dropPiece(3);
+  game.dropPiece(2);
+  game.dropPiece(2);
+  game.dropPiece(1);
   game.dropPiece(1);
   game.dropPiece(0);
   game.dropPiece(3);
   game.dropPiece(0);
   game.dropPiece(2);
   game.dropPiece(1);
-  game.dropPiece(2);
-  game.dropPiece(2);
-  game.dropPiece(3);
-  game.dropPiece(3);
-  game.dropPiece();
-  ASSERT_EQ(game.gameState(),O);
+  game.dropPiece(0);
+  ASSERT_EQ(game.gameState(),X);
 }
  TEST(PiezasTest, check_if_gameState_works_tie)
  {
  Piezas game;
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
- game.dropPiece();
+ game.dropPiece(3);
+ game.dropPiece(2);
+ game.dropPiece(3);
+ game.dropPiece(2);
+ game.dropPiece(3);
+ game.dropPiece(2);
+ game.dropPiece(1);
+ game.dropPiece(0);
+ game.dropPiece(1);
+ game.dropPiece(0);
+ game.dropPiece(1);
+ game.dropPiece(0);
  ASSERT_EQ(game.gameState(),Blank);
  }
 
  
-*/
